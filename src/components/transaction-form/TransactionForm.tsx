00d296046676a3a584e1e52680b8bb7c87e9ebac
Iryna <irynadauchyonas@gmail.com>
2026-04-10 13:48:09 +0300

    feat: add validation for transaction form (react-hook-form + yup)

--- src/components/transaction-form/TransactionForm.tsx
+++ src/components/transaction-form/TransactionForm.tsx
@@ -1,44 +1,87 @@
 import type { TransactionType } from "../../types/transaction";
 import Input from "../ui/Input";
-import { useRef } from "react";
-import classes from "./TransactionForm.module.css"
+import classes from "./TransactionForm.module.css";
+import { Controller, useForm, type FieldErrors } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+
+type FormData = {
+    amount: string;
+    category: string;
+    date: string;
+    note?: string;
+};
+
+const schema: yup.ObjectSchema<FormData> = yup.object({
+    amount: yup.string().required("Введіть суму").matches(/^$|^(?!\.)(\d+(\.\d*)?|\d+)$/, "Некоректна сума"),
+    category: yup.string().trim().required("Введіть категорію"),
+    date: yup.string().required("Оберіть дату"),
+    note: yup.string().optional(),
+}).required();
 
 type Props = {
     type: TransactionType
 }
 
 export default function TransactionForm({ type }: Props) {
-    const amountRef = useRef<HTMLInputElement>(null);
-    const categoryRef = useRef<HTMLInputElement>(null);
-    const dateRef = useRef<HTMLInputElement>(null);    
-    const noteRef = useRef<HTMLInputElement>(null);
+    const { register, control, handleSubmit, formState: { errors } } = useForm<FormData>({
+        resolver: yupResolver(schema)
+    });
+
+    const onValid = (data: FormData) => console.log(data);
+    const onInvalid = (errors: FieldErrors<FormData>): void => console.log(errors)
+
+    const handleChange = (value: string): string | undefined => {
+        const validationRegex = /^$|^(?!\.)(\d+(\.\d*)?|\d+)$/;
+        if (!validationRegex.test(value) && value !== "") {
+            return undefined;
+        }
+
+        return value;
+    }
 
     if (type === "transfer" || type === "debt") {
         return <div className={classes.placeholder}>Поки не реалізовано</div>;
     }
 
-    return <form className={classes.form}>
-        <Input
-            label="Сума"
-            id="amount"
-            ref={amountRef}
-            inputMode="decimal"
-        />
+    return <form className={classes.form} onSubmit={handleSubmit(onValid, onInvalid )}>
+        <Controller
+            name="amount"
+            control={control}
+            defaultValue=""
+            render={({ field }) => (
+                <Input
+                    label="Сума"
+                    id="amount"
+                    inputMode="decimal"
+                    value={field.value}
+                    error={errors.amount}
+                    onChange={(e) => {
+                        const newValue = handleChange(e.target.value);
+                        if (newValue !== undefined) {
+                            field.onChange(newValue);
+                        }
+                    }}
+                />
+            )} />
         <Input
             label="Категорія"
             id="category"
-            ref={categoryRef}
+            error={errors.category}
+            {...register("category")}
         />
         <Input
             label="Дата"
             id="date"
-            ref={dateRef}
             type="date"
+            error={errors.date}
+            {...register("date")}
         />
         <Input
             label="Коментар"
             id="note"
-            ref={noteRef}
+            error={errors.note}
+            {...register("note")}
         />
         <button className={classes.submit} type="submit" >Зберегти</button>
     </form>;
--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,18 +1,22 @@
 
 import type { ComponentPropsWithRef } from "react";
 import classes from "./Input.module.css";
+import type { FieldError } from "react-hook-form";
 
 type InputProps = ComponentPropsWithRef<"input"> & {
   label: string;
   id: string;
+  error?: FieldError
+  
 };
 
-export default function Input({ label, id, ...props }: InputProps) {
+export default function Input({ label, id, error, ...props }: InputProps) {
     return <div className={classes.wrapper}>
         <label className={classes.label} htmlFor={id}>{label}</label>
         <input className={classes.input}
             id={id}
             {...props}
         />
+        {error && <div className={classes.errorWrapper}><span>{error.message}</span></div>}
     </div>
 }
