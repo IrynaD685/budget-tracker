e1526baf1f0fa28b72ff3faea143453a30751383
Iryna <irynadauchyonas@gmail.com>
2026-04-14 13:19:34 +0300

    feat: Use localeStorage for reading and saving transactions

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./Header.tsx";
 import Navigation from "./Navigation.tsx";
@@ -7,8 +7,15 @@ import type { Transaction } from "../../types/transaction.ts";
 import { useNavigate } from "react-router-dom";
 
 export default function AppLayout() {
-    const [transactions, setTransactions] = useState<Transaction[]>([]);
+    const [transactions, setTransactions] = useState<Transaction[]>(() => {
+        const saved = localStorage.getItem("transactions");
+        return saved ? JSON.parse(saved) : [];
+    });
     const navigate = useNavigate();
+
+    useEffect(() => {
+        localStorage.setItem("transactions", JSON.stringify(transactions));
+    }, [transactions]);
 
     function addNewTransaction(transaction: Transaction) {
         setTransactions((prevTransactions) => [...prevTransactions, transaction]);
